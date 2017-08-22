d6d82a3cbdb80e0947edcb11344e641545301884
I.Roman <ilinroman.study@mail.ru>
2017-08-22 21:31:00 +0300

    Add files via upload

---
+++ Oracle Helper.sql
@@ -0,0 +1,38 @@
+-----------------------------------
+-- ВОССТАНОВЛЕНИЕ ДАННЫХ ТАБЛИЦЫ --
+-----------------------------------
+--Просмотр текущей мметки SCN
+select dbms_flashback.get_system_change_number from dual;
+--Запрос данных на заданный момент времени
+select *
+from t as of scn :scn;
+--Восстановление
+--1. разрешение на перемещение строк в БД
+alter table t enable row movement;
+--2. восстановление состояния таблицы к метке scn
+flashback table t to scn :scn;
+
+-----------------
+-- ПАРАЛЛЕЛИЗМ --
+-----------------
+--SID текущего сеанса
+select sid from v$mystat where rownum = 1;
+--или
+select sys_context('userenv','sid') from dual;
+
+--Проверка активации PDML (параллельное выполнение операции DML - INSERT, UPDATE, DELETE. MERGE)
+select pdml_enabled from v$session where sid= sys_context('userenv','sid');
+
+--Список параллельных сессий м транзакций
+select a.sid, a.program, b.start_time, b.used_ublk,
+b.xidusn ||'.'|| b.xidslot || '.' || b.xidsqn trans_id
+from v$session a, v$transaction b
+where a.taddr = b.addr
+and a.sid in ( select sid
+from v$px_session
+where qcsid = 258)
+order by sid;
+
+--Тип и количество параллельных действий
+select name, value from v$statname a, v$mystat b
+where a.statistic# = b.statistic# and name like '%parallel%';
